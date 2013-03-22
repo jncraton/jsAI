279e2c6da362d4cd0730fe24c68a5f9a8208f2cf
Jon Craton <jncraton@gmail.com>
2013-03-22 19:28:51 -0400

    added example

---
+++ example.js
@@ -0,0 +1,39 @@
+var ai = require('./lib/ai');
+
+with (ai) {
+    // Functions provide a higher order replacement for blocks
+    var simpleExpo = function() {
+        start_town();
+        build_finish(1, cc);
+        
+        for (var i = 1; i <= 20; i+=1) {
+            build_finish(i, scv);
+        }
+        stop();
+    };
+    
+    header('terran');
+    build_finish(9, scv);
+    build_start(1, depot);
+    build_start(15, scv);
+    
+    expand(1, simpleExpo);
+    
+    call(function addBarracks() {
+        build_start(1, rax);
+        farms_timing();
+        build_start(18, scv);
+        // This is JavaScript, so you can use loops
+        for (var i = 2; i <= 8; i+=1) {
+            build_start(i, rax);
+        }
+    });
+    
+    loop(function () {
+        train(24, marine);
+        expand(99, simpleExpo);
+        send_suicide(0);
+    });
+    
+    flush('build/terran.pyai');
+}
