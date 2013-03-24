5e65f49fa0d81115a6a48d355e9f30a67a8fe3b8
Jon Craton <jncraton@gmail.com>
2013-03-24 10:11:41 -0400

    updated example to show pick choosing a build order

--- example.js
+++ example.js
@@ -12,42 +12,26 @@ with (ai) {
         stop();
     };
     
+    var builds = {
+        'bbs': function () {
+            build_start(8, scv);
+            build_start(1, rax);
+            build_start(2, rax);
+            build_start(9, scv);
+            build_start(1, depot);
+            build_start(11, scv);
+        },
+        '14cc': function () {
+            build_start(9, scv);
+            build_start(1, depot);
+            build_start(14, scv);
+            expand(1, simpleExpo);
+        },
+    }
+    
     header('terran');
-    build_finish(9, scv);
-    build_start(1, depot);
-    build_start(15, scv);
     
-    expand(1, simpleExpo);
-    
-    call(function addBarracks() {
-        build_start(1, rax);
-        farms_timing();
-        build_start(18, scv);
-        // This is JavaScript, so you can use loops
-        for (var i = 2; i <= 8; i+=1) {
-            build_start(i, rax);
-        }
-    });
-
-    pick([
-        function() {
-            train(1, marine);
-            send_suicide(0);
-        },
-        function() {
-            train(4, marine);
-            send_suicide(0);
-        },
-        function() {
-            train(8, marine);
-            send_suicide(0);
-        }
-    ]);
-            
-    loop(function () {
-        train(8, marine);
-        send_suicide(0);
-    });
+    pick(builds);
     
     flush('build/terran.pyai');
 }
