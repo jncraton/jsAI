2669ff001dfcd95641b50e5c371b4b2e6a774016
Jon Craton <jncraton@gmail.com>
2013-05-25 20:53:27 -0400

    removed filename from flush method

--- example.js
+++ example.js
@@ -3,31 +3,31 @@ var bo = require('./lib/bo');
 
 with (ai) {
     header('terran');
-    
+
     // Functions provide a higher order replacement for blocks
     var simpleExpo = function() {
         start_town();
         build_finish(1, cc);
-        
+
         for (var i = 1; i <= 20; i+=1) {
             build_finish(i, scv);
         }
         stop();
     };
-    
+
     var builds = bo.parseFromFile('terranBuilds.md');
-    
+
     pick(builds);
-    
+
     pick({
         bio: function () {
             for (var i = 1; i <= 8; i += 1) {
                 build_start(i, rax);
             }
-            
+
             loop(function() {
                 train(24, marine);
-                
+
                 attack_clear();
                 attack_add(24, marine);
                 attack_prepare();
@@ -37,6 +37,6 @@ with (ai) {
             });
         }
     });
-    
-    flush('build/terran.pyai');
+
+    flush();
 }
