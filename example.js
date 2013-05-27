1be103ab129ecdd7cbdff23db0a6247ed91830ab
Jon Craton <jncraton@gmail.com>
2013-05-27 10:40:43 -0400

    cleanup

--- example.js
+++ example.js
@@ -5,17 +5,6 @@ var midgame = require('./lib/midgame');
 with (ai) {
     header('terran');
 
-    // Functions provide a higher order replacement for blocks
-    var simpleExpo = function() {
-        start_town();
-        build_finish(1, cc);
-
-        for (var i = 1; i <= 20; i+=1) {
-            build_finish(i, scv);
-        }
-        stop();
-    };
-
     var builds = bo.parseFromFile('terranBuilds.md');
 
     pick(builds);
